2e5b9cc8fddef8b23d0571bec393303391bd688f
devalmon <phlood@yandex.ru>
2020-05-22 23:36:17 +0300

    Initial Commit

---
+++ Multithreading/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multithreading
+//
+//  Created by Alexey Baryshnikov on 22.05.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
